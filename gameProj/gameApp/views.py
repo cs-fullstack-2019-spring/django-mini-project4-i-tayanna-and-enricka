7f3a0df3dd20a5f524991d55ff485887a8ccafca
EnrickaM <marshallenricka@yahoo.com>
2019-03-05 10:35:42 -0600

    endpoints

--- gameProj/gameApp/views.py
+++ gameProj/gameApp/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django .http import HttpResponse
 
 # Create your views here.
+# this function will take you to the home page of the website.
+def index(request):
+    return  render(request,"gameApp/index.html")
+
+def newuser(request):
+
